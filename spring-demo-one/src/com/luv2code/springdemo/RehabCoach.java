b0f0eacf8bdd30ac00694ee50a2c391e22446881
yunjae <juj7347@naver.com>
2022-01-11 17:12:51 +0900

    fortune interface, implementation class

--- spring-demo-one/src/com/luv2code/springdemo/RehabCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/RehabCoach.java
@@ -6,4 +6,10 @@ public class RehabCoach implements Coach {
 	public String getDailyWorkout() {
 		return "Stretch for 30 minutes";
 	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
