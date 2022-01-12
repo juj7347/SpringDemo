dd85ce046398732cca2e18afb62affc3bae2f2b0
yunjae <juj7347@naver.com>
2022-01-12 23:30:07 +0900

    setter injection done

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -8,6 +8,15 @@ public class CricketCoach implements Coach {
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}
+	
+	// setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minutes";
