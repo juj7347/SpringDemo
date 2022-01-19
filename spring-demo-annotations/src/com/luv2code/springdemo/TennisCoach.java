802bd4c41874972fc01bd24e05bccdf1f4ade370
yunjae <juj7347@naver.com>
2022-01-19 22:49:15 +0900

    implementing multiple beans on same interface using qualifier

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 
 	// define a default constructor
@@ -14,12 +17,13 @@ public class TennisCoach implements Coach {
 	}
 	
 	// define a setter method
+	/*
 	@Autowired
-	public void setFortuneService(FortuneService theFortuneService) {
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">>  TennisCoach: inside set method");
 		fortuneService = theFortuneService;
 	}
-	
+	*/
 	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
