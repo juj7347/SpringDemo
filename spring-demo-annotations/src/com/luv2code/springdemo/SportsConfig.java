8392423e5f0b32c24bf61cf6a70ca68a75010d55
yunjae <juj7347@naver.com>
2022-01-20 17:44:53 +0900

    java source code config init

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SportsConfig.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.luv2Code.springdemo")
+public class SportsConfig {
+	
+}
