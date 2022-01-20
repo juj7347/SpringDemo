eaf8506870ed6e0e492bf1b3ac159564cb2b59ad
yunjae <juj7347@naver.com>
2022-01-20 17:30:26 +0900

    jar file addded for postconstruct, predestroy, code for its demo

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +18,20 @@ public class TennisCoach implements Coach {
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	
+	@PostConstruct
+	public void doMyStartStuff() {
+		System.out.println(">> TennisCoach: inside of doMuStartStuff()");
+	}
+	
+	// define my destroy method
+	
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanUpStuff()");
 	}
 	
 	// define a setter method
